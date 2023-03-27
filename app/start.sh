dcd01db7e0c2142326c6a1d2d213d12b748da5ad
parkjunhada <junha6316@gmail.com>
2023-03-27 11:09:49 +0900

    추가: jars 추가

--- app/start.sh
+++ app/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 pip install -r /app/requirements.txt
 sleep 10 
-/opt/bitnami/spark/bin/spark-submit --master spark://spark-master:7077 --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.1.2,com.datastax.spark:spark-cassandra-connector_2.12:3.1.0 --conf spark.ui.enabled=true /app/data-analysis.py
+/opt/bitnami/spark/bin/spark-submit --master spark://spark-master:7077 --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.1.2,com.datastax.spark:spark-cassandra-connector_2.12:3.1.0 --jars /app/jars/spark-redis_2.12-3.1.0-SNAPSHOT.jar --conf spark.ui.enabled=true /app/data-analysis.py
