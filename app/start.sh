af8d5cc80acdacfceef2a4fb7b60b3d525392c61
parkjunhada <junha6316@gmail.com>
2023-04-01 13:08:41 +0900

    수정: start.sh 수정

--- app/start.sh
+++ app/start.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+apt-get update && apt-get install -y sudo
+rm -rf /opt/bitnami/spark/.ivy2/cache
+chown -R $USER:$USER /.cache/pip
+chmod -R 755 /.cache/pip
 pip install -r /app/requirements.txt
 sleep 10 
-/opt/bitnami/spark/bin/spark-submit --master spark://spark-master:7077 --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.1.2,com.datastax.spark:spark-cassandra-connector_2.12:3.1.0 --jars /app/jars/spark-redis_2.12-3.1.0-SNAPSHOT.jar --conf spark.ui.enabled=true /app/data-analysis.py
+/opt/bitnami/spark/bin/spark-submit --master spark://spark-master:7077 \
+    --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.0.0,com.datastax.spark:spark-cassandra-connector_2.12:3.0.0 \
+    --jars /app/jars/commons-pool2-2.0.jar,/app/jars/jedis-3.4.1.jar,/app/jars/spark-redis_2.12-3.0.0.jar,/app/jars/slf4j-api-1.7.30.jar \
+    --conf spark.ui.enabled=true \
+     /app/data-analysis.py
