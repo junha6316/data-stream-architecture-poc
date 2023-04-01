1cf416c81e2bf750775d5e8a1f104f24ca847cd5
parkjunhada <junha6316@gmail.com>
2023-04-01 13:08:11 +0900

    수정: 분석 코드 수정

--- app/data-analysis.py
+++ app/data-analysis.py
@@ -2,105 +2,125 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
 
-# Create a SparkSession
-spark = SparkSession.builder.appName("crm-analysis").getOrCreate()
 
-# Define the schema for the CRM messages
-crm_message_schema = StructType([StructField("message_id", IntegerType(), True)])
+class MyApp:
+    def run(self):
+        spark = (
+            SparkSession.builder.appName("crm-analysis")
+            .config("spark.redis.host", "host.docker.internal")
+            .config("spark.redis.port", "6379")
+            .config("spark.redis.timeout", 10000)
+            .config("redis.timeout", 10000)
+            .getOrCreate()
+        )
+        spark.sparkContext.setLogLevel("DEBUG")
 
-# Define the window duration for the analysis
-window_duration = "14 days"
+        crm_message_ids = (
+            spark.readStream.format("kafka")
+            .option("kafka.bootstrap.servers", "kafka:29092")
+            .option("subscribe", "crm_message")
+            .option("startingOffsets", "earliest")
+            .load()
+            .selectExpr(
+                "CAST(key AS STRING)",
+            )
+            .select(col("key").cast("int").alias("message_id"))
+        )
 
-# Read CRM message ids from Kafka
-crm_message_ids = (
-    spark.readStream.format("kafka")
-    .option("kafka.bootstrap.servers", "localhost:9092")
-    .option("subscribe", "crm_message")
-    .option("startingOffsets", "earliest")
-    .load()
-    .select(col("key").cast("int").alias("message_id"))
-)
+        distinct_message_ids = crm_message_ids.distinct()
+        distinct_message_ids.createOrReplaceTempView(
+            "distinct_message_ids"
+        )  # SQL로 작업하기 위해 dataframe으로
 
-# Read CRM messages from Redis and filter by message ID
-crm_messages = (
-    spark.read.format("org.apache.spark.sql.redis")
-    .option("table", "crm_messages")
-    .option("key.column", "message_id")
-    .option(
-        "predicates",
-        f"message_id IN ({crm_message_ids.select('message_id').distinct().collect()})",
-    )
-    .load()
-)
+        crm_messages = (
+            spark.read.format("org.apache.spark.sql.redis")
+            .option("table", "crm_messages")
+            .option("infer.schema", "true")
+            .option(
+                "predicates",
+                f"message_id IN (SELECT message_id FROM {distinct_message_ids})",
+            )
+            .load()
+        )
 
-# Read product data from Redis and filter by target product ID
-product_data = (
-    spark.read.format("org.apache.spark.sql.redis")
-    .option("table", "product_data")
-    .option("key.column", "id")
-    .option(
-        "predicates",
-        f"id IN ({crm_messages.select('target_product_id').distinct().collect()})",
-    )
-    .load()
-)
+        product_data = (
+            spark.read.format("org.apache.spark.sql.redis")
+            .option("table", "products")
+            .option("infer.schema", "true")
+            .option("predicates", "id IN (SELECT target_product_id FROM crm_messages)")
+            .load()
+            .withColumnRenamed("id", "product_id")
+        )
 
-# Read purchase data from Redis and filter by target product ID and purchase date
-purchase_data = (
-    spark.read.format("org.apache.spark.sql.redis")
-    .option("table", "purchase_data")
-    .option("key.column", "id")
-    .option(
-        "predicates",
-        f"product_id IN ({crm_messages.select('target_product_id').distinct().collect()})",
-    )
-    .load()
-    .filter(col("created_at").between(window.start, window.end))
-)
+        window_duration = "14 days"
+        target_window = window(col("created_at"), window_duration)
 
-# Read user data from Redis and join with purchase data
-user_data = (
-    spark.read.format("org.apache.spark.sql.redis")
-    .option("table", "user_data")
-    .option("key.column", "phone_number")
-    .option(
-        "predicates",
-        f"phone_number IN ({crm_messages.select('phone_number').distinct().collect()})",
-    )
-    .load()
-    .withColumnRenamed("phone_number", "crm_phone_number")
-    .join(
-        purchase_data.select("user_id", "crm_phone_number"), "crm_phone_number", "inner"
-    )
-    .withColumnRenamed("user_id", "id")
-    .drop("crm_phone_number")
-)
+        purchase_data = (
+            spark.read.format("org.apache.spark.sql.redis")
+            .option("table", "purchase_logs")
+            .option("infer.schema", "true")
+            .option(
+                "predicates",
+                f"product_id IN ({crm_messages.select('target_product_id').distinct().collect()})",
+            )
+            .load()
+            .filter(col("created_at").between(target_window.start, target_window.end))
+            .join(product_data, "product_id", "inner")
+        )
 
-# Join CRM messages with user, product, and purchase data
-joined_data = (
-    crm_messages.join(user_data, "phone_number")
-    .join(product_data, "id")
-    .join(purchase_data, "user_id", "inner")
-)
+        user_data = (
+            spark.read.format("org.apache.spark.sql.redis")
+            .option("table", "users")
+            .option("infer.schema", "true")
+            .option(
+                "predicates",
+                f"phone_number IN ({crm_messages.select('phone_number').distinct().collect()})",
+            )
+            .load()
+            .withColumnRenamed("phone_number", "crm_phone_number")
+            .withColumnRenamed("id", "user_id")
+            .join(
+                purchase_data.select(
+                    "user_id",
+                ),
+                "user_id",
+                "inner",
+            )
+            .withColumnRenamed("crm_phone_number", "phone_number")
+        )
 
-# Define the window for the analysis
-window = window(col("created_at"), window_duration)
+        joined_data: DataFrame = crm_messages.join(user_data, "phone_number").join(
+            purchase_data, "user_id", "inner"
+        )
 
-# Perform analysis on the data
-crm_purchase_counts = joined_data.groupBy("target_product_id").agg(
-    countDistinct(
-        when(col("created_at").between(window.start, window.end), col("user_id"))
-    ).alias("crm_purchase_count"),
-    sum(when(col("created_at").between(window.start, window.end), col("price"))).alias(
-        "crm_total_purchase_value"
-    ),
-)
+        joined_data.show()
 
-# Write the results back to Redis
-crm_purchase_counts.write.format("org.apache.spark.sql.redis").option(
-    "table", "crm_purchase_counts"
-).option("key.column", "target_product_id").mode("overwrite").save()
+        crm_purchase_counts = joined_data.groupBy("target_product_id").agg(
+            countDistinct(
+                when(
+                    col("created_at").between(target_window.start, target_window.end),
+                    col("user_id"),
+                )
+            ).alias("crm_purchase_count"),
+            sum(
+                when(
+                    col("created_at").between(target_window.start, target_window.end),
+                    col("price"),
+                )
+            ).alias("crm_total_purchase_value"),
+        )
 
-# Start the streaming query
-query = crm_purchase_counts.writeStream.format("console").outputMode("complete").start()
-query.awaitTermination()
+        crm_purchase_counts.write.format("org.apache.spark.sql.redis").option(
+            "table", "crm_purchase_counts"
+        ).option("key.column", "target_product_id").mode("overwrite").save()
+
+        query = (
+            crm_purchase_counts.writeStream.format("console")
+            .outputMode("complete")
+            .start()
+        )
+        query.awaitTermination()
+
+
+if __name__ == "__main__":
+    MyApp().run()
